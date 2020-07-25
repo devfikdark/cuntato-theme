076678612a9347d184922d8b50b2efa5834e7a67
zubayerhimel <zhimel007@gmail.com>
2020-07-25 23:31:55 +0600

    fixed some issues, implemented copy button

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,19 @@ $(function () {
     constrainWidth: false,
     autoTrigger: true,
   });
+
+  $("#copy-btn").click(function () {
+    var copyText = document.getElementById("project-credential");
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+    showMessage("Code copied to clipboard");
+  });
+
+  function showMessage(message) {
+    M.toast({ html: message, classes: "rounded brown" });
+  }
 });
